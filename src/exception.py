04cd1864e2fc9dc0aa81c88ea5ab179054de589b
Usmanuuu52 <usmankhanuuu52@gmail.com>
2024-07-18 13:14:20 +0500

    add data_ingestion.py

--- src/exception.py
+++ src/exception.py
@@ -8,15 +8,14 @@ def error_message_detail(error,error_detail:sys):
         file_name,exp_tb.tb_lineno,str(error))
     
     return error_message
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
-    class CustomException(Exception):
-        def __init__(self,error_message,error_detail:sys):
-            super().__init__(error_message)
-            self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
-
-        def __str__(self) -> str:
-            return self.error_message
+    def __str__(self) -> str:
+        return self.error_message
         
 
 
